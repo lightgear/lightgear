24ef63e43ab55b023b915d2c2c21ac1e22356398
Donato Rotunno <ralf57@Bandusia.local>
2015-04-22 22:19:50 +0200

    Add Artisan based installer

---
+++ app/Console/Commands/LightgearInstaller.php
@@ -0,0 +1,95 @@
+<?php namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
+use App\User;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class LightgearInstaller extends Command {
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'lightgear:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = "Lightgear install utility";
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function fire()
+    {
+        // is it really a new installation? 
+        if ($this->isInstalled()) {
+            return $this->error('Lightgear is already installed. Empty your Database to re-install it.');
+        }
+
+        // run DB migrations
+        $this->runMigrations();
+
+        // create admin user
+        $this->createAdmin();
+
+        // create sample content
+        $this->addSampleContent();
+    }
+
+    protected function isInstalled()
+    {
+        return Schema::hasTable('migrations');
+    }
+
+    protected function runMigrations()
+    {
+        $this->info('Preparing database.');
+        $this->call('migrate:install');
+        $this->call('migrate');
+        $this->info('Done.');
+    }
+
+    protected function createAdmin()
+    {
+        $this->info('Creating administrator account.');
+
+        $username = $this->ask('Administrator username: ');
+        $email = $this->ask('Administrator email: ');
+        $password = $this->secret('Administrator password: ');
+        $passwordConfirm = $this->secret('Re-type administrator password: ');
+
+        if ($password === $passwordConfirm) {
+            $user = new User();
+            $user->name = $username;
+            $user->email = $email;
+            $user->password = \Hash::make($password);
+
+            if ($user->save()) {
+                return $this->info('Admin user created successfully.');
+            }
+        } else {
+            $this->error('The password you entered don\'t match. Please try again.');
+            $this->createAdmin();
+        }
+    }
+
+    protected function addSampleContent()
+    {
+        $question = 'Do you wish to install sample content? [yes|no]';
+
+        if ($this->confirm($question, false))
+        {
+            $this->call('db:seed');
+        }
+    }
+
+}
